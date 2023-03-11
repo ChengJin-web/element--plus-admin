f5cd63fd9e2faf739a1816a7151d2df77f5df1bf
ChengJin <57589172@qq.com>
2023-03-12 01:08:53 +0800

    添加页面

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,41 @@
 import {fileURLToPath, URL} from 'node:url'
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        vueJsx(),
-        createSvgIconsPlugin({
-            iconDirs: [fileURLToPath(new URL('src/assets/icons/svg', import.meta.url))],
-            symbolId: 'icon-[dir]-[name]'
-        })
-    ],
-    resolve: {
-        alias: {
-            '~': fileURLToPath(new URL('./', import.meta.url)),
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    server: {
-        port: 80,
-        host: true,
-        open: false,
-        proxy: {
-            '/dev-api': {
-                target: 'http://vue.ruoyi.vip/',
-                changeOrigin: true,
-                rewrite: (p) => p.replace(/^\/dev-api/, '')
+export default defineConfig(({mode}) => {
+    // eslint-disable-next-line no-undef
+    const env = loadEnv(mode, process.cwd())
+    const {VITE_APP_ENV} = env
+    return {
+        base: VITE_APP_ENV === 'production' ? '/' : '/',
+        plugins: [
+            vue(),
+            vueJsx(),
+            createSvgIconsPlugin({
+                iconDirs: [fileURLToPath(new URL('src/assets/icons/svg', import.meta.url))],
+                symbolId: 'icon-[dir]-[name]'
+            })
+        ],
+        resolve: {
+            alias: {
+                '~': fileURLToPath(new URL('./', import.meta.url)),
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            },
+            extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+        },
+        server: {
+            port: 80,
+            host: true,
+            open: false,
+            proxy: {
+                '/dev-api': {
+                    target: 'http://vue.ruoyi.vip/',
+                    changeOrigin: true,
+                    rewrite: (p) => p.replace(/^\/dev-api/, '')
+                }
             }
         }
     }
